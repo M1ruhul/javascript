6fdcfd3163c12bd0b3003ee1a5631c7d21de2b7d
M1ruhul <mr1ruhul@gmail.com>
2024-08-29 23:46:43 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// let con1 = '33';
+// console.log(typeof con1); 
+// let con2 = 45;
+// con1 = parseFloat(con1);
+// console.log(typeof con1);
+// let mot = con1 + con2;
+// console.log(mot);
+// let num1 = '44.2376'
+// let num2 = 34.24;
+// num1 = parseFloat(num1)
+// total = num1 + num2;
+// console.log(total);
+// let num1 = 88;
+// let num2 = 40;
+// let total = num1 % num2;
+// console.log(total);
+// let sum = -43;
+// let sum2 = 46;
+// let newvalue = Math.abs(sum);
+// console.log(newvalue);
+// let a = 1;
+// let b = 0;
+// if(1>0){
+// console.log('ami programming er boss hobe'); 
+// }
+// if(1<0){
+//     console.log('ana marijun khatimun')
+// }
+
+// if(1<0){
+// console.log('ami programming er boss hobe'); 
+// }
+// else {
+//     console.log('ana marijun khatimun')
+// }
+// vul ase kisu
+// if(1=0){
+// console.log('ami tumake vlobashi');
+// }
+// else if(1<0){
+// console.log('tumi amake vlobasho');
+// }
+// else{
+//     console.log('doesn0t match')
+// }
+let fdName = 'shuv0';
+switch (fdName) {
+    case 'mehedi':
+    console.log('ekhono halay Dohar');
+        break;
+    case 'nesar':
+        console.log('upwork niye busy');
+
+    default:
+        console.log('amar log in code astese na');
+        break;
+}
